cbdbc95d77371fa7ac2eccf7b2565e0af4da1d37
Shahriar Islam <shahriar-2023216004@cs.du.ac.bd>
2026-01-09 13:55:12 +0600

    main Application

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
 
+
 const updateSW = registerSW({
   onNeedRefresh() {
     if (confirm('New content available. Reload?')) {
